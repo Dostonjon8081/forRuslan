f78755cdfc6a8842fdc4e58aff621d6b07af152d
omad omad <omadov022@gmail.com>
2021-10-29 11:21:06 +0500

    auth fix

--- app/src/main/java/uz/fizmasoft/dyhxx/activity/MainActivity.kt
+++ app/src/main/java/uz/fizmasoft/dyhxx/activity/MainActivity.kt
@@ -15,12 +15,13 @@ import com.google.android.play.core.install.model.UpdateAvailability
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import uz.fizmasoft.dyhxx.R
 import uz.fizmasoft.dyhxx.base.BaseActivity
 import uz.fizmasoft.dyhxx.core_fragment.home.HomeViewModel
 import uz.fizmasoft.dyhxx.databinding.ActivityMainBinding
 import uz.fizmasoft.dyhxx.helper.db.CarEntity
+import uz.fizmasoft.dyhxx.helper.network.NetworkResult
 import uz.fizmasoft.dyhxx.helper.network.model.AllCars
 import uz.fizmasoft.dyhxx.helper.util.*
 
@@ -30,11 +31,12 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
 
     private val viewModel: HomeViewModel by viewModels()
     private lateinit var appUpdateManager: AppUpdateManager
-    private val listener: InstallStateUpdatedListener? = InstallStateUpdatedListener { installState ->
-        if (installState.installStatus() == InstallStatus.DOWNLOADED) {
+    private val listener: InstallStateUpdatedListener? =
+        InstallStateUpdatedListener { installState ->
+            if (installState.installStatus() == InstallStatus.DOWNLOADED) {
 //            showSnackBarForCompleteUpdate()
+            }
         }
-    }
 
     override fun setupItems() {
         appUpdateManager = AppUpdateManagerFactory.create(this)
@@ -66,38 +68,52 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         ) {
 
             val editPref = getPref(this@MainActivity).edit()
-//            CoroutineScope(Dispatchers.IO).async {
+            CoroutineScope(Dispatchers.IO).launch {
                 editPref.putString(PREF_TOKEN_KEY, intent.data.toString().substring(22))
-                    .commit()
-//            }.onAwait
-            loadDataFromApi()
+                    .apply()
+            }
+            loadDataFromApi(intent.data.toString().substring(22))
         }
     }
 
-    private fun loadDataFromApi() {
+    private fun loadDataFromApi(token: String) {
         val editPref = getPref(this@MainActivity).edit()
-        viewModel.getUserIdApi(getPref(this).getString(PREF_TOKEN_KEY, "")!!)
+        viewModel.getUserIdApi(token)
         viewModel.responseUserIdApi.observe(this) {
             try {
-                editPref.putString(PREF_USER_ID_KEY, it.data?.user_id.toString()).commit()
 
+                when (it) {
+                    is NetworkResult.Success -> {
 
-                viewModel.allCarsApi(AllCars(getPref(this).getString(PREF_USER_ID_KEY, "")!!))
-                viewModel.responseAllCarsApi.observe(this, EventObserver { data ->
+                        editPref.putString(PREF_USER_ID_KEY, it.data?.user_id.toString()).apply()
+                        viewModel.allCarsApi(AllCars(it.data?.user_id.toString()))
+                        viewModel.responseAllCarsApi.observe(this, EventObserver { data ->
 
-                    if (data.data?.data != null && data.data.status != 404) {
-                        for (item in data.data.data) {
-                            viewModel.insertCarDB(
-                                CarEntity(
-                                    0,
-                                    item.passport,
-                                    item.tex_passport
-                                )
-                            )
-                        }
+                            when (data) {
+                                is NetworkResult.Success -> {
+                                    if (data.data?.data != null && data.data.status != 404) {
+                                        for (item in data.data.data) {
+                                            viewModel.insertCarDB(
+                                                CarEntity(0, item.passport, item.tex_passport)
+                                            )
+                                        }
 
+                                    }
+                                }
+                                is NetworkResult.Error -> {}
+                                is NetworkResult.Loading -> {}
+                            }
+
+                        })
                     }
-                })
+                    is NetworkResult.Error -> {
+                        carToast(this, "Ro'yhatdan o'tish amalga oshmadi. Iltimos qaytadan o'ting")
+                    }
+                    is NetworkResult.Loading -> {
+                        carToast(this, "Loading...")
+                    }
+                }
+
             } catch (e: Exception) {
                 carToast(this, e.toString())
             }
@@ -117,13 +133,15 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
 
         appUpdateInfoTask?.addOnSuccessListener { appUpdateInfo ->
             if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
 
                 appUpdateManager.startUpdateFlowForResult(
                     appUpdateInfo,
                     AppUpdateType.IMMEDIATE,
                     this,
-                    IN_APP_UPDATE_REQUEST_CODE)
+                    IN_APP_UPDATE_REQUEST_CODE
+                )
             } else {
 //                logd("No Update available")
             }
@@ -132,11 +150,14 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
 //            carToast(this, "No updated")
         }
     }
-    private fun updateInProgress(){
-        appUpdateManager?.appUpdateInfo?.addOnSuccessListener {updateInfo->
-            if (updateInfo.updateAvailability()==UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS){
-                appUpdateManager?.startUpdateFlowForResult(updateInfo,AppUpdateType.IMMEDIATE,this,
-                    IN_APP_UPDATE_REQUEST_CODE)
+
+    private fun updateInProgress() {
+        appUpdateManager?.appUpdateInfo?.addOnSuccessListener { updateInfo ->
+            if (updateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                appUpdateManager?.startUpdateFlowForResult(
+                    updateInfo, AppUpdateType.IMMEDIATE, this,
+                    IN_APP_UPDATE_REQUEST_CODE
+                )
             }
 
         }
@@ -145,11 +166,17 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode== IN_APP_UPDATE_REQUEST_CODE ){
-            when(resultCode){
-                Activity.RESULT_OK->{carToast(this,"Update done")}
-                Activity.RESULT_CANCELED->{carToast(this,"Update Canceled")}
-                ActivityResult.RESULT_IN_APP_UPDATE_FAILED->{carToast(this,"Fail Update")}
+        if (requestCode == IN_APP_UPDATE_REQUEST_CODE) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    carToast(this, "Update done")
+                }
+                Activity.RESULT_CANCELED -> {
+                    carToast(this, "Update Canceled")
+                }
+                ActivityResult.RESULT_IN_APP_UPDATE_FAILED -> {
+                    carToast(this, "Fail Update")
+                }
             }
         }
     }
