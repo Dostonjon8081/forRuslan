7d0835e5d480d50dcd05a5ed5773eaa82957aee6
Zuhriddinjon Rayimjonov <zuhriddinjonrayimjonov@gmail.com>
2021-11-01 16:47:31 +0500

    Crashlite

--- app/src/main/java/uz/fizmasoft/dyhxx/App.kt
+++ app/src/main/java/uz/fizmasoft/dyhxx/App.kt
@@ -2,6 +2,7 @@ package uz.fizmasoft.dyhxx
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 
 
@@ -9,6 +10,7 @@ import dagger.hilt.android.HiltAndroidApp
 class App : Application(){
     override fun onCreate() {
         super.onCreate()
+             FirebaseCrashlytics.getInstance().log("Salom Boooy") // Force a crash
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
