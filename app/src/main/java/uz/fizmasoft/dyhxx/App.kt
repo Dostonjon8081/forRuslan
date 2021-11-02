14b8db99931f192bc98f5b215dbc57007c82a95c
Zuhriddinjon Rayimjonov <zuhriddinjonrayimjonov@gmail.com>
2021-11-02 12:19:48 +0500

    add firebase Crashlytics

--- app/src/main/java/uz/fizmasoft/dyhxx/App.kt
+++ app/src/main/java/uz/fizmasoft/dyhxx/App.kt
@@ -2,15 +2,13 @@ package uz.fizmasoft.dyhxx
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
-import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 
 
 @HiltAndroidApp
-class App : Application(){
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
-             FirebaseCrashlytics.getInstance().log("Salom Boooy") // Force a crash
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
